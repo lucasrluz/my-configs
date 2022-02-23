cc2aca4480ab681498e664d552d286bec1af5bd8
lucasrluz <lucasr.luzbr@gmail.com>
2022-02-23 17:05:00 -0300

    script de inicialização

---
+++ start/start.sh
@@ -0,0 +1,15 @@
+sleep 1
+
+xrandr --output HDMI-2 --mode 1920x1080 --left-of eDP-1
+
+sleep 1
+
+~/.fehbg &
+
+sleep 1
+
+xset s off -dpms
+
+sleep 1
+
+setxkbmap -model abnt2 -layout br -variant abnt2
